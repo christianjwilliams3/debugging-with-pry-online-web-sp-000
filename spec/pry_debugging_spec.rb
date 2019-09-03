86c912b48a0e6b7c934d3b96ec8977392ff46eec
christian williams <christian.williamsj@gmail.com>
2019-09-03 21:50:19 +0000

    Automatically backed up by Learn

--- spec/pry_debugging_spec.rb
+++ spec/pry_debugging_spec.rb
@@ -1,9 +1,9 @@
 require 'pry'
 require './lib/pry_debugging.rb'
 
-def plus_two(num) do 
-	it "takes in a number as an argument and returns the sum of that number and 2" do
-		plus_two(3 == 5)
-	end
+def plus_two(num)
+ if num + 2 
+   return 5
+  num
 binding.pry
 end
